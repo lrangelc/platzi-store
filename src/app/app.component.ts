8cf10fb203732c3aa5d7823c4309badd828e1501
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-03 16:24:47 -0600

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = [
+    'Luis',
+    'Reyna',
+    'Rocio',
+    'Rodrigo',
+    '🍎',
+    '🍏',
+    '🍇',
+    '🍌',
+    '🍑',
+    '😎',
+  ];
+
+  addItem(): void {
+    this.items.push(this.title);
+  }
+
+  deleteItem(index: number): void {
+    this.items.splice(index, 1);
+  }
 }
