6391d46056992795c99364f1675806d950ca0968
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-04 14:50:11 -0600

    remove home.html

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
-import { BannerComponent } from './components/banner/banner.component';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from './../shared/shared.module';
 import { BannComponent } from './components/bann/bann.component';
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent, BannComponent],
+  declarations: [HomeComponent, BannComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
