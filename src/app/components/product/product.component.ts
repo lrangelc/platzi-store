18668bba0b7e1227eb60544cab9a8ebb311c41c0
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-03 21:41:26 -0600

    add css

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy,
+} from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
 
   @Output() productClicked: EventEmitter<string> = new EventEmitter<string>();
 
+  constructor() {
+    console.log('constructor');
+  }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log('ngOnChanges');
+  //   console.log(changes);
+  // }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
   addCart(product: Product): void {
     console.log(`add to the cart ${product.title}`);
 
