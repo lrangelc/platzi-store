c71dd46a0c813150e53ff8052209251c7244c972
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-07 11:29:10 -0600

    add FormControl

--- src/app/shared/components/fotter/fotter.component.ts
+++ src/app/shared/components/fotter/fotter.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-fotter',
   templateUrl: './fotter.component.html',
-  styleUrls: ['./fotter.component.scss']
+  styleUrls: ['./fotter.component.scss'],
 })
 export class FotterComponent implements OnInit {
+  emailField: FormControl;
 
-  constructor() { }
+  constructor() {
+    this.emailField = new FormControl('', [
+      Validators.minLength(4),
+      Validators.required,
+      Validators.email,
+      Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/),
+    ]);
 
-  ngOnInit(): void {
+    // this.emailField.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
   }
 
+  ngOnInit(): void {}
+
+  sendMail(): void {
+    if (this.emailField.valid) {
+      console.log(this.emailField.value);
+    }
+  }
 }
