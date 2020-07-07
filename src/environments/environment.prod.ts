881aaeaa1a85774fda3f6c0c6a95d80a21097fb9
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-07 17:49:05 -0600

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  url_api : 'http://platzi-store.herokuapp.com'
+  url_api: 'http://platzi-store.herokuapp.com',
+  firebaseConfig = {
+    apiKey: 'AIzaSyCi13uiGgGg8w133VFOmDUA7jLLSya55bw',
+    authDomain: 'blogeekplatzilarc.firebaseapp.com',
+    databaseURL: 'https://blogeekplatzilarc.firebaseio.com',
+    projectId: 'blogeekplatzilarc',
+    storageBucket: 'blogeekplatzilarc.appspot.com',
+    messagingSenderId: '596958338322',
+    appId: '1:596958338322:web:2640197f9b5789550ae7cb',
+    measurementId: 'G-L0LBB513EH',
+  },
 };
