d398453e960cbf0e21e3a19e0f78ce5009293553
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-01 10:23:35 -0600

    change component to container

--- src/app/containers/products/products.container.ts
+++ src/app/containers/products/products.container.ts
@@ -4,30 +4,30 @@ import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss'],
+  templateUrl: './products.container.html',
+  styleUrls: ['./products.container.scss'],
 })
-export class ProductsComponent implements OnInit {
-
+// tslint:disable-next-line: component-class-suffix
+export class ProductsContainer implements OnInit {
   products: Product[];
 
   constructor(private productsService: ProductsService) {}
 
-  fetchProducts() {
+  ngOnInit(): void {
+    // this.products = this.productsService.getAllProducts();
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
     this.productsService.getAllProducts().subscribe((products) => {
       console.log(products);
-      for (let product of products) {
+      for (const product of products) {
         if (product.image.length === 0) {
           product.image = 'assets/images/banner-3.jpg';
         }
       }
       this.products = products;
     });
-  }
-
-  ngOnInit(): void {
-    // this.products = this.productsService.getAllProducts();
-    this.fetchProducts();
   }
 
   clickProduct(id: string): void {
