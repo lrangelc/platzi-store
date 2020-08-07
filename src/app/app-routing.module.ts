73600f28699fe3f80ec96735a99959f27522cc03
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-07 11:34:19 -0600

    add preload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { DemoComponent } from './components/demo/demo.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { PreloadService } from './core/services/preload.service';
 
 import { AdminGuard } from './guards/admin.guard';
 
@@ -24,14 +25,17 @@ const routes: Routes = [
         path: 'home',
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomeModule),
+        data: { preload: true },
       },
       {
         path: 'products',
         component: ProductsContainer,
+        data: { preload: true },
       },
       {
         path: 'products/:id',
         component: ProductDetailComponent,
+        data: { preload: true },
       },
       {
         path: 'contact',
@@ -53,22 +57,22 @@ const routes: Routes = [
     canActivate: [AdminGuard],
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
+      data: { preload: true },
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: '**',
     component: PageNotFoundComponent,
   },
-
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
+      preloadingStrategy: PreloadService,
     }),
   ],
   exports: [RouterModule],
